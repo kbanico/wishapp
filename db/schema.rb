edac910786d6aaf0505ab984e0cb6801e118d210
kbanico <code4uguam@gmail.com>
2017-04-04 02:54:12 +0000

    add user_id to wish model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402095457) do
+ActiveRecord::Schema.define(version: 20170404021021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20170402095457) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "donation",   default: 0
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_wishes_on_user_id", using: :btree
   end
 
 end
